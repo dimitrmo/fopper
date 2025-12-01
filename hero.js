5243a129777b90920f278cdbabfc7ecbdcb8f61a
Dimitrios Mouratidis <76875895+dimitrmo@users.noreply.github.com>
2025-12-01 10:17:22 +0200

    Update hero.js

--- hero.js
+++ hero.js
@@ -4,11 +4,7 @@ export class HeroComponent extends HTMLElement {
     connectedCallback() {
         this.data = JSON.parse(this.getAttribute('data') || '{}');
 
-        this.innerHTML = `
-            <h1>
-                Hello, ${this.#getValue('name', 'sklave')}!
-            </h1>
-            
+        this.innerHTML = `            
             <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="${this.#getValue('color', 'red')}" />
             </svg>
